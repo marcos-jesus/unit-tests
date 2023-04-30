72b6ee02313f3937c3680f15aad1578dabfc2d79
Marcos Jesus <marcosjesus.dev@gmail.com>
2023-04-30 05:00:51 -0300

    Implement test with Axios request

---
+++ src/components/__tests__/Request.spec.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+
+import { mount } from '@vue/test-utils'
+import { describe, it, expect } from 'vitest'
+import Welcome from '@/components/WelcomeItem.vue'
+
+describe('Should get users', () => {
+  it('Should mount component', () => {
+    const wrapper = mount(Welcome)
+
+    expect(wrapper.vm.$el).toBeTruthy()
+  })
+
+  it('Should return request users', () => {
+    const mock = new MockAdapter(axios)
+
+    mock.onGet('/users').reply(200, {
+      users: [
+        { id: 1, name: 'Marcos'},
+        { id: 2, name: 'Buddy'}
+      ]
+    })
+
+    axios.get('/users').then((response) => {
+      expect(response.data.users).toEqual([
+        { id: 1, name: 'Marcos'},
+        { id: 2, name: 'Buddy'}
+      ])
+    })
+  })
+})
